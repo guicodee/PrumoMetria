e160211601dd184c10667e836ce10a5ebfbe7b19
guicodee <guilhermedfgo12@gmail.com>
2026-03-10 20:36:59 -0300

    feat: implement ApplicationUser entity and configure DBContext.

---
+++ PrumoMetria/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PrumoMetria.Entities;
+
+namespace PrumoMetria.Data;
+
+public class ApplicationDbContext(
+    DbContextOptions<ApplicationDbContext> options
+) : IdentityDbContext<ApplicationUser>(options);
---
+++ PrumoMetria/Entities/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PrumoMetria.Entities;
+
+public class ApplicationUser : IdentityUser
+{
+     public string? Name { get; set; }
+}
