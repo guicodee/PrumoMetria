4b1a6f4101fcaa5be7cbc60e0d06993ed775dd39
guicodee <guilhermedfgo12@gmail.com>
2026-03-12 20:57:43 -0300

    feat: add jwt settings class

---
+++ PrumoMetria/Settings/JwtSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace PrumoMetria.Settings;
+
+public class JwtSettings
+{
+    public string Key { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public int ExpiresInHours { get; set; } = 1;
+    public string LoginProvider { get; set; } = string.Empty;
+    public string RefreshTokenName { get; set; } = string.Empty;
+}
