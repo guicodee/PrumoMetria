efaec55fa8d7ed47e1214c650aef8a9db1577f2c
guicodee <guilhermedfgo12@gmail.com>
2026-03-17 21:33:58 -0300

    feat: register study entities as DbSets in DbContext

--- PrumoMetria/Data/ApplicationDbContext.cs
+++ PrumoMetria/Data/ApplicationDbContext.cs
@@ -6,4 +6,17 @@ namespace PrumoMetria.Data;
 
 public class ApplicationDbContext(
     DbContextOptions<ApplicationDbContext> options
-) : IdentityDbContext<ApplicationUser>(options);
+) : IdentityDbContext<ApplicationUser>(options)
+{
+    public DbSet<StudyPlan> StudyPlans { get; set; }
+    public DbSet<Subject> Subjects { get; set; }
+    public DbSet<Content> Contents { get; set; }
+    public DbSet<StudySession> StudySessions { get; set; }
+    public DbSet<Tasks> Tasks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
