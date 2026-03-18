a7179f7595257caa85ac9f3e1ee92d9b67da2946
guicodee <guilhermedfgo12@gmail.com>
2026-03-17 21:33:32 -0300

    feat: link StudyPlan entity to ApplicationUser

--- PrumoMetria/Entities/ApplicationUser.cs
+++ PrumoMetria/Entities/ApplicationUser.cs
@@ -5,4 +5,7 @@ namespace PrumoMetria.Entities;
 public class ApplicationUser : IdentityUser
 {
      public string? Name { get; set; }
+     public bool IsPremium { get; set; } = false;
+
+     public ICollection<StudyPlan> StudyPlans { get; set; } = [];
 }
