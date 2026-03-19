eeb0783dd1e23565644cbebed12bbb26b2ce6e16
guicodee <guilhermedfgo12@gmail.com>
2026-03-18 21:46:01 -0300

    feat: implement StudyPlanExtensions for entity-to-dto mapping

---
+++ PrumoMetria/Helpers/ToDto.cs
@@ -0,0 +1,19 @@
+﻿using PrumoMetria.Dto.StudyPlans;
+using PrumoMetria.Entities;
+
+namespace PrumoMetria.Helpers;
+
+    public static class StudyPlanExtensions
+    {
+        public static StudyPlanDTO ToDTO(this StudyPlan studyPlan)
+        {
+            return new StudyPlanDTO
+            {
+                Id = studyPlan.Id,
+                Name = studyPlan.Name,
+                Color = studyPlan.Color,
+                Description = studyPlan.Description,
+                CreatedAt = studyPlan.CreatedAt
+            };
+        }
+    }
