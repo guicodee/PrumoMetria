5db0009b0f1f78b537e12251c072d8ec5127f590
guicodee <guilhermedfgo12@gmail.com>
2026-03-19 22:56:56 -0300

    feat: implement generic ServiceResult for standardized service responses

---
+++ PrumoMetria/Services/StudyResult.cs
@@ -0,0 +1,31 @@
+﻿namespace PrumoMetria.Services.StudyPlans;
+
+public class ServiceResult<T>
+{
+    public T? Data { get; private set; }
+    public string? Error { get; private set; }
+    public bool IsSuccess { get; private set; }
+    public int StatusCode { get; private set; }
+
+    private ServiceResult() { }
+
+    public static ServiceResult<T> Success(T data, int statusCode = 200)
+    {
+        return new ServiceResult<T>
+        {
+            Data = data,
+            IsSuccess = true,
+            StatusCode = statusCode
+        };
+    }
+
+    public static ServiceResult<T> Fail(string error, int statusCode)
+    {
+        return new ServiceResult<T>
+        {
+            Error = error,
+            IsSuccess = false,
+            StatusCode = statusCode
+        };
+    }
+}
