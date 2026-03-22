f424b86c0bfbe8955206585ccae1a2c23663561e
guicodee <guilhermedfgo12@gmail.com>
2026-03-22 10:26:52 -0300

    feat: implement StudyPlanController with full endpoints

---
+++ PrumoMetria/Controllers/StudyPlanController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrumoMetria.Dto.StudyPlans;
+using PrumoMetria.Services.StudyPlans;
+
+namespace PrumoMetria.Controllers;
+
+[Route("api/studyplan")]
+public class StudyPlanController : BaseController
+{
+    private readonly IStudyPlanService _service;
+
+    public StudyPlanController(IStudyPlanService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("{studyPlanId}")]
+    public async Task<IActionResult> GetPlanById([FromRoute] Guid studyPlanId)
+    {
+        var userId = GetUserId();
+        
+        var result = await _service.GetPlanById(userId, studyPlanId);
+
+        if (!result.IsSuccess)
+            return StatusCode(result.StatusCode, result.Error);
+
+        return Ok(result.Data);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreatePlan([FromBody] CreateStudyPlanDTO studyPlan)
+    {
+        var userId = GetUserId();
+        
+        var result = await _service.Create(userId, studyPlan);
+
+        if (!result.IsSuccess)
+            return StatusCode(result.StatusCode, result.Error);
+
+        return CreatedAtAction(nameof(CreatePlan), new { Id = result.Data!.Id}, result.Data);
+    }
+
+    [HttpPut("{studyPlanId}")]
+    public async Task<IActionResult> UpdatePlan(
+        [FromRoute] Guid studyPlanId, 
+        [FromBody] UpdateStudyPlanDTO studyPlan)
+    {
+        var userId = GetUserId();
+        
+        var result = await _service.Update(userId, studyPlanId, studyPlan);
+
+        if (!result.IsSuccess)
+            return StatusCode(result.StatusCode, result.Error);
+            
+        return Ok(result.Data);
+    }
+
+    [HttpDelete("{studyPlanId}")]
+    public async Task<IActionResult> DeletePlan([FromRoute] Guid studyPlanId)
+    {
+        var userId = GetUserId();
+        
+        var result = await _service.Delete(userId, studyPlanId);
+
+        if (!result.IsSuccess)
+            return StatusCode(result.StatusCode, result.Error);
+
+        return NoContent();
+    }
+}
