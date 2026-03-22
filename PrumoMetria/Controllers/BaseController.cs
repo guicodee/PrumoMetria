b6013f01451074686c7a6b73c51553b354d4a03e
guicodee <guilhermedfgo12@gmail.com>
2026-03-22 10:18:44 -0300

    feat: add BaseController with shared user context logic

---
+++ PrumoMetria/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrumoMetria.Controllers;
+
+[Authorize]
+[ApiController]
+public class BaseController : ControllerBase
+{
+    protected string GetUserId()
+    {
+        return User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+    }
+}
