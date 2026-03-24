3a07c2fa5872792503c26d2f47813157b82e5ed7
guicodee <guilhermedfgo12@gmail.com>
2026-03-23 21:31:47 -0300

    feat: add subject repository and interface

---
+++ PrumoMetria/Repositories/Subject/ISubjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using PrumoMetria.Entities;
+
+namespace PrumoMetria.Repositories;
+
+public interface ISubjectRepository
+{
+    Task AddSubject(Subject subject);
+    Task UpdateSubject(Subject subject);
+    Task DeleteSubject(Subject subject);
+    Task<Subject?> GetSubjectById(Guid subjectId);
+    Task<List<Subject>> GetSubjects(Guid studyPlanId);
+    Task<int> CountSubjectsByStudyPlanId(Guid studyPlanId);
+}
---
+++ PrumoMetria/Repositories/Subject/SubjectRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrumoMetria.Data;
+using PrumoMetria.Entities;
+
+namespace PrumoMetria.Repositories;
+
+public class SubjectRepository : ISubjectRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public SubjectRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task AddSubject(Subject subject)
+    {
+        await _context.Subjects.AddAsync(subject);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateSubject(Subject subject)
+    {
+        subject.UpdatedAt = DateTime.UtcNow;
+        
+        _context.Subjects.Update(subject);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteSubject(Subject subject)
+    {
+        _context.Subjects.Remove(subject);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Subject?> GetSubjectById(Guid subjectId)
+    {
+        return await _context.Subjects
+            .Where(subject => subject.Id == subjectId)
+            .Include(subject => subject.Contents)
+            .Include(subject => subject.StudySessions)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<List<Subject>> GetSubjects(Guid studyPlanId)
+    {
+        return await _context.Subjects
+            .Where(subject => subject.StudyPlanId == studyPlanId)
+            .Include(subject => subject.Contents)
+            .OrderByDescending(subject => subject.CreatedAt)
+            .ToListAsync();
+    }
+    
+    public async Task<int> CountSubjectsByStudyPlanId(Guid studyPlanId)
+    {
+        return await _context.Subjects
+            .CountAsync(subject => subject.StudyPlanId == studyPlanId);
+    }
+}
