b93f98ee7f2f62b0fa0055b14c786d5f77795f3d
guicodee <guilhermedfgo12@gmail.com>
2026-03-25 21:51:49 -0300

    feat: add content repository implementation

---
+++ PrumoMetria/Repositories/Content/ContentRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrumoMetria.Data;
+using PrumoMetria.Entities;
+
+namespace PrumoMetria.Repositories;
+
+public class ContentRepository : IContentRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public ContentRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task AddContent(Content content)
+    {
+        await _context.Contents.AddAsync(content);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateContent(Content content)
+    {
+        content.UpdatedAt = DateTime.UtcNow;
+
+        _context.Contents.Update(content);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteContent(Content content)
+    {
+        _context.Contents.Remove(content);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Content?> GetContentById(Guid contentId)
+    {
+        return await _context.Contents
+            .FirstOrDefaultAsync(c => c.Id == contentId);
+    }
+
+    public async Task<List<Content>> GetContentList(Guid contentId)
+    {
+        return await _context.Contents
+            .Where(content => content.Id == contentId)
+            .OrderBy(content => content.CreatedAt)
+            .ToListAsync();
+    }
+
+    public async Task<int> CountContentBySubjectId(Guid subjectId)
+    {
+        return await _context.Contents
+            .CountAsync(content => content.SubjectId == subjectId);
+    }
+}
