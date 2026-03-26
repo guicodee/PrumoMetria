618f3364e56ec9e5b29445d264f71153652982d8
guicodee <guilhermedfgo12@gmail.com>
2026-03-26 20:46:19 -0300

    feat: add content service interface

---
+++ PrumoMetria/Services/Contents/IContentService.cs
@@ -0,0 +1,12 @@
+﻿using PrumoMetria.Dto.Contents;
+
+namespace PrumoMetria.Services.Contents;
+
+public interface IContentService
+{
+    Task<ServiceResult<ContentDTO?>> Create(string userId, Guid subjectId, CreateContentDTO content);
+    Task<ServiceResult<ContentDTO?>> Update(string userId, Guid contentId, UpdateContentDTO content);
+    Task<ServiceResult<bool>> Delete(string userId, Guid contentId);
+    Task<ServiceResult<ContentDTO?>> GetContentById(string userId, Guid contentId);
+    Task<ServiceResult<List<ContentDTO>>> GetContentList(string userId, Guid subjectId);
+}
