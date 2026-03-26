7cef8a4597aeb0763cf349ce245065910fa290ad
guicodee <guilhermedfgo12@gmail.com>
2026-03-25 21:51:34 -0300

    feat: add content repository interface

---
+++ PrumoMetria/Repositories/Content/IContentRepository.cs
@@ -0,0 +1,13 @@
+﻿using PrumoMetria.Entities;
+
+namespace PrumoMetria.Repositories;
+
+public interface IContentRepository 
+{
+    Task AddContent(Content content);
+    Task UpdateContent(Content content);
+    Task DeleteContent(Content content);
+    Task<Content?> GetContentById(Guid contentId);
+    Task<List<Content>> GetContentList(Guid subjectId);
+    Task<int> CountContentBySubjectId(Guid subjectId);
+}
