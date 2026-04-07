cdb34161d814f1a23296c166a0fbfb37dd38175f
guicodee <guilhermedfgo12@gmail.com>
2026-04-07 18:09:45 -0300

    feat: add study task repository interface

---
+++ PrumoMetria/Repositories/StudyTasks/IStudyTasksRepository.cs
@@ -0,0 +1,13 @@
+﻿using PrumoMetria.Entities;
+
+namespace PrumoMetria.Repositories;
+
+public interface IStudyTasksRepository
+{
+    Task AddStudyTasks(StudyTask task);
+    Task UpdateStudyTasks(StudyTask task);
+    Task DeleteStudyTasks(StudyTask task);
+    Task<StudyTask?> GetStudyTasksById(Guid taskId);
+    Task<List<StudyTask>> GetStudyTaskss(Guid studyPlanId);
+    Task<int> CountStudyTaskByStudyPlanId(Guid studyPlanId);
+}
