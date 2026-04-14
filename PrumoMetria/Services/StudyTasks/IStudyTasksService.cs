4ace070861ba57361d7bd7619b1fe1612cffb653
guicodee <guilhermedfgo12@gmail.com>
2026-04-13 21:37:36 -0300

    refactor: modify of contentId for studyPlanId

--- PrumoMetria/Services/StudyTasks/IStudyTasksService.cs
+++ PrumoMetria/Services/StudyTasks/IStudyTasksService.cs
@@ -4,7 +4,7 @@ namespace PrumoMetria.Services.StudyTasks;
 
 public interface IStudyTasksService
 {
-    Task<ServiceResult<StudyTaskDTO>> Create(string userId,Guid contentId, CreateStudyTasksDTO studyTask);
+    Task<ServiceResult<StudyTaskDTO>> Create(string userId,Guid studyPlanId, CreateStudyTasksDTO studyTask);
     Task<ServiceResult<StudyTaskDTO>> Update(string userId, Guid studyTaskId, UpdateStudyTaskDTO studyTask);
     Task<ServiceResult<bool>> Delete(string userId, Guid studyTaskId);
     Task<ServiceResult<StudyTaskDTO>> GetStudyTaskById(string userId, Guid studyTaskId);
